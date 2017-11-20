29eeb8b27bc033ae210029fa22dd1f7ae42c02d0
shernandez <shernandez@E0070>
2017-11-20 13:03:14 +0100

    ....

--- Encamina.SPO.Intelligence.Services/CelebrityService.cs
+++ Encamina.SPO.Intelligence.Services/CelebrityService.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CongnitveManager
-{
+{//
     public class CelebrityService
     {
         public ContentResult MakeAnalysisCelebrity(Stream image)
